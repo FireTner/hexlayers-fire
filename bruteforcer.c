67ff0eb539357b32f7b4aaa729839a8405a26472
FireTner <49531229+FireTner@users.noreply.github.com>
2022-09-15 06:55:05 +0200

    cleanup, fixed comp

--- bruteforcer.c
+++ bruteforcer.c
@@ -7,22 +7,19 @@
 typedef __m128i v16n;
 typedef unsigned char u8;
 
-v16n t;
+v16n t, one, zero;
 
-// can be negative
-v16n ncomp(v16n a, v16n b, u8 m) {
-    if(m) return _mm_sub_epi8(a, b);
-    return _mm_and_si128(a, _mm_cmpgt_epi8(a, b));
+v16n comp(v16n a, v16n b, u8 m) {
+    if(m) return _mm_max_epi8(_mm_sub_epi8(a, b), 0);
+    return _mm_and_si128(a, _mm_cmpgt_epi8(b, a));
 }
 
 // generates look up table
 size_t genLut(v16n *lut, size_t lutsize) {
     // very good error handling
-    if(lutsize < 1024) return -1;
+    //if(lutsize < 1024) return -1;
     
     size_t newsize = 0;
-    const v16n zero = _mm_set1_epi8(0);
-    const v16n _mm_one = _mm_set1_epi8(0xFF);
     v16n x;
 
     for(int i = 0; i < 1024; i++) {
@@ -32,22 +29,19 @@ size_t genLut(v16n *lut, size_t lutsize) {
         u8 b  = (i & 0xF0) >> 4;
 
 
-        x = _mm_max_epi8(zero, _mm_max_epi8(
-            ncomp(_mm_set1_epi8(a), t, ma),
-            ncomp(t, _mm_set1_epi8(b), mb)
-        ));
+        x = _mm_max_epi8(
+            comp(t, _mm_set1_epi8(a), ma),
+            comp(_mm_set1_epi8(b), t, mb)
+        );
 
-        int isFound = 0;
-
-        for(int j = 0; j < newsize; j++)
-            if( _mm_test_all_zeros(_mm_xor_si128(x, lut[j]), _mm_one) )
-                isFound = 1;
-
-        if(isFound==0) lut[newsize++] = x;
+        int j = 0;
+        for(j; j < newsize; j++)
+            if( _mm_test_all_zeros(_mm_xor_si128(x, lut[j]), one) )
+                break;
+        if(j==newsize) lut[newsize++] = x;
 
         if(newsize==303) {
-            printf("%d %d %d %d\n", ma, mb, a, b);
-        }
+            printf("%d %d %d %d\n", ma
     }
 
     printf("genLut finished\n");
@@ -56,11 +50,11 @@ size_t genLut(v16n *lut, size_t lutsize) {
 
 void findSeq(const v16n goal, const v16n *lut, const size_t lutsize, int *result) {
     int counter[50] = { [0 ... 49] = -1 };
-    const v16n _mm_one = _mm_set1_epi8(0xFF);
     v16n x;
 
     do {
-        for(int i = 0; i < 50; i++) {;
+        // increment counter
+        for(int i = 0; i < 50; i++) {
             if(counter[i]++!=lutsize) break;
             counter[i] = 0;
         }
@@ -72,28 +66,40 @@ void findSeq(const v16n goal, const v16n *lut, const size_t lutsize, int *result
         
         // check equality
         x = _mm_xor_si128(x, goal);
-    } while(!_mm_testz_si128(x, _mm_one));
+    } while(!_mm_testz_si128(x, one));
 
-    printf("%d\n", counter[0]);
-    printf("%d\n", counter[1]);
-    //printf("findSeq finished\n");
+    printf("0; %2d\n", counter[0]);
+    printf("1; %2d\n", counter[1]);
+    printf("findSeq finished\n");
 }
 
-/*void outputSeq(unsigned int* sequence) {
-    return;
-}*/
+void outputSeq(unsigned int* sequence) {
+    return; // TODO
+}
 
 int main(int argc, char **argv) {
+    // initialize constants and variables
     t = _mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+    one = _mm_set1_epi8(1);
+    zero = _mm_set1_epi8(0);
+    
     v16n goal = _mm_set_epi8(15,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14);
-    size_t lutsize = 1024;
-    v16n *lut = malloc(lutsize * sizeof(v16n));
-    int *result = malloc(50 * sizeof(int));
+    
+    size_t olutsize = 1024;
+    v16n *lut = (v16n *)malloc(olutsize * sizeof(v16n));
+    int *result = (int *)malloc(50 * sizeof(int));
 
-    lutsize = genLut(lut, lutsize);
-    printf("n: %zu\n", lutsize);
+    // generate lut
+    lutsize = genLut(lut, olutsize);
+    printf("lutsize: %zu\n", lutsize);
+    if(olutsize>lutsize)
+        lut = (v16n *)realloc(lut, lutsize);
+    
+    // find the sequence
     findSeq(goal, lut, lutsize, result);
-    //outputSeq(counter);
+    
+    // output the result
+    outputSeq(result);
 
     return 0;
 }
